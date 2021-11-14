ef891cffb94d1da69027baebf38058989a9a8ed0
Marc <marc.artco@gmail.com>
2021-11-14 19:15:29 +0100

    primera part acabada

--- nivell 1/ex4.php
+++ nivell 1/ex4.php
@@ -1,12 +1,34 @@
 <?php
     //variables
-    $x=2;
+    $x=7;
     $y=4;
-    $n=2.5;
-    $m=1.5;
-
+    $n=7.5;
+    $m=2.5;
+    //operacions amb X i Y
+    echo "Operacions amb X i Y"."<br>";
     echo "Valor de x = ".$x."<br>";
     echo "Valor de y = ".$y."<br>";
+
+    echo "Suma de ".$x." + ".$y." = ".$x+$y."<br>";
+    echo "Resta de ".$x." - ".$y." = ".$x-$y."<br>";
+    echo "Producte de ".$x." * ".$y." = ".$x*$y."<br>";
+    echo "Mòdul de ".$x." % ".$y." = ".$x%$y."<br>";
+    
+    //operacions amb N i M
+    echo "Operacions amb N i M"."<br>";
+    echo "Valor de N = ".$n."<br>";
+    echo "Valor de M = ".$m."<br>";
+
+    echo "Suma de ".$n." + ".$m." = ".$n+$m."<br>";
+    echo "Resta de ".$n." - ".$m." = ".$n-$m."<br>";
+    echo "Producte de ".$n." * ".$m." = ".$n*$m."<br>";
+    echo "Mòdul de ".$n." % ".$m." = ".$n%$m."<br>";
+
+
+
+
+
+
 
 
 
