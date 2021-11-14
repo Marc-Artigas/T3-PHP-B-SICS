3d1b4d4f2a2c79a16369357acd03183c4c77feaf
Marc <marc.artco@gmail.com>
2021-11-14 18:20:31 +0100

    ex2 finalitzat

--- nivell 1/Exercici 2/ex2.php
+++ nivell 1/Exercici 2/ex2.php
@@ -5,7 +5,10 @@
 
     echo strtoupper($var1). "<br>"; //majúscules
     echo strlen($var1). "<br>"; // longitud de la variable
-    
+    echo strrev($var1). "<br>"; //reverse string
+
+    $var2="Aquest és el curs de PHP.";
+    echo $var1." ".$var2;
 
 
 
