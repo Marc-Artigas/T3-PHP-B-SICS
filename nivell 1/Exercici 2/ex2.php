8b5d085402ece71456e8f8694f3ff4d35f31b67d
Marc <marc.artco@gmail.com>
2021-11-14 18:13:39 +0100

    majusc i longitud de la variable

--- nivell 1/Exercici 2/ex2.php
+++ nivell 1/Exercici 2/ex2.php
@@ -1,7 +1,11 @@
 <?php
 
     $var1="Hello world!";
-    echo $var1;
+    echo $var1. "<br>";
+
+    echo strtoupper($var1). "<br>"; //majúscules
+    echo strlen($var1). "<br>"; // longitud de la variable
+    
 
 
 
